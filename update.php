615eb2f65527728ebe5562b1d40cc42826d90b9f
Ridoy <sheakh35-2091@diu.edu.bd>
2022-08-20 23:22:32 +0600

    Some modifications on update.php files

--- update.php
+++ update.php
@@ -1,3 +1,22 @@
 <?php 
     echo "Something has been updated on!";
+    class database{
+        private $db_servername = "localhost";
+		private $db_username   = "root";
+		private $db_password   = "";
+		private $db_name       = "shop_db";
+
+		private $connection    = false;
+		private $conn          = "";
+		private $result        = array();
+
+        //Function to connect the database!
+        public function __construct()
+        {
+            if(!$this->connection)
+            {
+                $this->conn = new mysqli();
+            }
+        } 
+    }
 ?>
